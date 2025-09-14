5f6d23acad37db7589f540efde2e7be15c391818
Tyler <108450341+JW33R@users.noreply.github.com>
2025-09-14 16:35:17 -0400

    Add files via upload

---
+++ CW Is This UpperCase.py
@@ -0,0 +1,4 @@
+#This program checks to see if the argument entered is uppercase and returns the result
+def isThisUpperCase(x):
+    return x.isupper()
+print(isThisUpperCase("wadawdwa"))
---
+++ CW RemoveWS.py
@@ -0,0 +1,4 @@
+#This program removes White Space from strings
+def noSpace(x):
+    return x.replace(" ", "")
+print(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"))
